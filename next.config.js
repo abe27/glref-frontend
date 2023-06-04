548e3ef75b4d389d3dea21c6e48323791a7c0782
Taweechai Yuenyang <krumii.it@gmail.com>
2023-06-04 10:53:50 +0700

    update env file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    APP_NAME: `${process.env.APP_NAME}`,
+    URL_HOST: `${process.env.API_HOST}`,
+    API_HOST: `${process.env.API_HOST}/api/v1`,
+    JWT_SECRET: process.env.JWT_SECRET,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
